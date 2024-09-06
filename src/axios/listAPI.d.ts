d59cc2b6b55db8200e26556b5e88a75708d7b46c
hannnnxiiii <hanxi_lin@outlook.com>
2024-09-06 13:52:53 +0800

    fix: 补全typescript语法

---
+++ src/axios/listAPI.d.ts
@@ -0,0 +1,4 @@
+// src/axios/listAPI.d.ts
+declare module "@/axios/listAPI.js" {
+  export function getNowShowingAPI(): Promise<any> // 根据实际返回类型进行修改
+}
--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -4,12 +4,40 @@ import { getNowShowingAPI } from "@/axios/listAPI.js"
 // 当前页数
 const currentPage = ref(1)
 // 正在热映列表
-const nowShowingList = ref([])
+const nowShowingList = ref<Item[]>([])
+// 正在热映对象接口
+interface Rating {
+  average: number
+}
+interface Images {
+  large: string
+  medium: string
+  small: string
+}
+interface Directors {
+  name: string
+  id: string
+}
+interface Casts {
+  name: string
+  id: string
+}
+interface Item {
+  rating: Rating
+  id: string
+  images: Images
+  title: string
+  original_title: string
+  year: string
+  collect_count: number
+  directors: Directors[]
+  casts: Casts[]
+}
 // 获取正在热映
 const getNowShowing = async () => {
   const res = await getNowShowingAPI()
   nowShowingList.value = res.data.subjects
-  nowShowingList.value.forEach((item: object) => {
+  nowShowingList.value.forEach((item: Item) => {
     item.rating.average = item.rating.average / 2
   })
 }
@@ -29,7 +57,7 @@ const detailDialog = ref(null)
 // 弹窗倒计时
 let timer: number | null = null
 // 当前悬停的对象
-const currentItem = ref({})
+const currentItem = ref<Item>({} as Item)
 // 悬停对象的坐标
 const itemPosition = ref({
   x: 0,
@@ -42,14 +70,17 @@ const castsStr = ref("")
 // 悬浮框显示与否
 const dialogVisible = ref("none")
 // 鼠标移入，1s后显示详情弹窗
-const showDetailDialog = (event, item) => {
+const showDetailDialog = (event: MouseEvent, item: Item) => {
   timer = setTimeout(() => {
     currentItem.value = item
-    console.log(currentItem.value)
     directorsStr.value = item.directors.map((item) => item.name).join(" / ")
     castsStr.value = item.casts.map((item) => item.name).join(" / ")
-    itemPosition.value.x = event.toElement.getBoundingClientRect().x + 125
-    itemPosition.value.y = event.toElement.getBoundingClientRect().y
+    const target = event.target as HTMLElement
+    if (target) {
+      itemPosition.value.x = target.getBoundingClientRect().x + 125
+      itemPosition.value.y = target.getBoundingClientRect().y
+    }
+
     dialogVisible.value = "block"
   }, 500)
 }
@@ -69,11 +100,14 @@ const hideDetailDialog = () => {
     <!-- 左侧 -->
     <div class="w-[675px] h-[3000px] bg-green-300">
       <!-- 正在热映 -->
-      <div class="h-[324px] bg-blue-300 overflow-hidden">
+      <div
+        class="h-[324px] bg-blue-300 overflow-hidden"
+        v-if="nowShowingList.length !== 0"
+      >
         <!-- 头部 -->
         <div class="h-[36px] bg-yellow-100 border-b mb-[18px]">
-          <div class="h-[25px] flex justify-between">
-            <div class="flex items-center w-[260px] justify-between h-[25px]">
+          <div class="h-[25px] flex justify-between items-center">
+            <div class="flex items-end w-[260px] justify-between h-[25px]">
               正在热映
               <div
                 class="text-[13px] text-[#208ACC] hover:text-white hover:bg-[#208ACC]"
@@ -86,15 +120,27 @@ const hideDetailDialog = () => {
                 <RouterLink to="/">即将上映>></RouterLink>
               </div>
             </div>
-            <div class="flex w-[90px] justify-between items-center">
-              <div class="text-[12px]">{{ currentPage }} / 8</div>
+            <div class="flex w-[90px] justify-between items-end">
+              <div class="text-[12px]">
+                {{ currentPage }} / {{ Math.ceil(nowShowingList.length / 5) }}
+              </div>
               <el-pagination
                 layout="prev, next"
-                :total="50"
+                :page-size="5"
+                :total="nowShowingList.length"
                 size="small"
                 v-model:current-page="currentPage"
                 @next-click="handleNext"
                 @prev-click="handlePrev"
+                style="
+                  --el-pagination-button-width-small: 18px;
+                  --el-pagination-button-height-small: 18px;
+                  --el-pagination-border-radius: 18px;
+                  padding-left: 0;
+                  --el-pagination-bg-color: #6d99d2;
+                  --el-pagination-button-color: white;
+                  --el-pagination-hover-color: black;
+                "
               />
             </div>
           </div>
@@ -135,33 +181,6 @@ const hideDetailDialog = () => {
               暂无评分
             </div>
             <el-button type="primary" style="height: 24px">选座购票</el-button>
-            <!-- 悬浮框 -->
-            <!-- <div
-              class="absolute w-[250px] h-[200px] z-[100] bg-white top-0 left-[125px] border p-[20px] hidden"
-              ref="detailDialog"
-            >
-              <span>你的名字</span>
-              <span class="ml-[10px] text-gray-400 text-[12px]">2024</span>
-              <div class="flex">
-                <el-rate
-                  v-model="item.rating.average"
-                  disabled
-                  size="small"
-                  style="--el-rate-icon-margin: 0px"
-                />
-                <span
-                  class="mx-[5px] text-[#ff9900] leading-[24px] inline-block"
-                >
-                  5
-                </span>
-                <span class="text-[12px] leading-[24px]">(1000人评分)</span>
-              </div>
-              <div class="text-[12px] my-[10px]">129分钟 美国</div>
-              <div class="text-[12px] my-[10px]">
-                导演导演导演导演导演导演导演导演导演导演导演
-              </div>
-              <div class="text-[12px]">主演</div>
-            </div> -->
           </div>
         </div>
       </div>
@@ -173,9 +192,11 @@ const hideDetailDialog = () => {
         v-if="currentItem.rating"
       >
         <span>{{ currentItem.title }}</span>
-        <span v-if="currentItem.original_title" class="ml-[5px]">{{
-          currentItem.original_title
-        }}</span>
+        <span
+          v-if="currentItem.original_title !== currentItem.title"
+          class="ml-[5px]"
+          >{{ currentItem.original_title }}</span
+        >
         <span class="ml-[10px] text-gray-400 text-[12px]">{{
           currentItem.year
         }}</span>
