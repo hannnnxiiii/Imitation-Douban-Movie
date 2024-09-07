9b6ca63aa19fb3bd88b7f957309e5bb4d45b79be
hannnnxiiii <hanxi_lin@outlook.com>
2024-09-07 15:10:32 +0800

    学校台式提交测试

--- components.d.ts
+++ components.d.ts
@@ -14,16 +14,8 @@ declare module 'vue' {
     ElPagination: typeof import('element-plus/es')['ElPagination']
     ElPopover: typeof import('element-plus/es')['ElPopover']
     ElRate: typeof import('element-plus/es')['ElRate']
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
-    IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
-    IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
-    IconEcosystem: typeof import('./src/components/icons/IconEcosystem.vue')['default']
-    IconSupport: typeof import('./src/components/icons/IconSupport.vue')['default']
-    IconTooling: typeof import('./src/components/icons/IconTooling.vue')['default']
     RecentPopularComp: typeof import('./src/components/RecentPopularComp.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    TheWelcome: typeof import('./src/components/TheWelcome.vue')['default']
-    WelcomeItem: typeof import('./src/components/WelcomeItem.vue')['default']
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-// import { onMounted } from "vue"
-// onMounted(() => {
-//   const meta = document.createElement("meta")
-//   meta.name = "referrer"
-//   meta.content = "no-referrer"
-//   document.head.appendChild(meta)
-// })
+import { onMounted } from "vue";
+onMounted(() => {
+  const meta = document.createElement("meta");
+  meta.name = "referrer";
+  meta.content = "no-referrer";
+  document.head.appendChild(meta);
+});
 </script>
 
 <template>
